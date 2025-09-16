0b640b66aac814278302098be8d750578d043f13
aarushiattray <aarushiattray@gmail.com>
2025-09-15 23:06:14 -0400

    Added my name, class, and github repo url

--- src/app/Labs/page.tsx
+++ src/app/Labs/page.tsx
@@ -1,26 +1,37 @@
 import Link from "next/link";
+
 export default function Labs() {
- return (
-   <div id="wd-labs">
-     <h1>Labs</h1>
-     <ul>
-       <li>
-         <Link href="/Labs/Lab1" id="wd-lab1-link">
-           Lab 1: HTML Examples </Link>
-       </li>
-       <li>
-         <Link href="/Labs/Lab2" id="wd-lab2-link">
-           Lab 2: CSS Basics </Link>
-       </li>
-       <li>
-         <Link href="/Labs/Lab3" id="wd-lab3-link">
-           Lab 3: JavaScript Fundamentals </Link>
-       </li>
-       <li>
+  return (
+    <div id="wd-labs">
+      <h1>Labs</h1>
+      <p id="wd-student-name">Aarushi Attray, CS 4550</p> 
+      <ul>
+        <li>
+          <Link href="/Labs/Lab1" id="wd-lab1-link">
+            Lab 1: HTML Examples
+          </Link>
+        </li>
+        <li>
+          <Link href="/Labs/Lab2" id="wd-lab2-link">
+            Lab 2: CSS Basics
+          </Link>
+        </li>
+        <li>
+          <Link href="/Labs/Lab3" id="wd-lab3-link">
+            Lab 3: JavaScript Fundamentals
+          </Link>
+        </li>
+        <li>
           <Link href="/" id="wd-kambaz-link">
             Kambaz
           </Link>
-        </li>  
-     </ul>
-   </div>
-);}
+        </li>
+        <li>
+          <Link href="https://github.com/aarushiattray/kambaz-next-js" id="wd-github" target="_blank" rel="noopener noreferrer">
+            GitHub Repository
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
