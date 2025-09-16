7534e697ea189ad328721d768c98f1f0a71ebbea
aarushiattray <aarushiattray@gmail.com>
2025-09-15 22:50:07 -0400

    new change

--- src/app/(Kambaz)/Courses/[cid]/layout.tsx
+++ src/app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,13 +1,15 @@
 import { ReactNode } from "react";
 import CourseNavigation from "./Navigation";
 
-type CoursesLayoutProps = {
+export default function CoursesLayout({
+  children,
+  params,
+}: {
   children: ReactNode;
-  params: { cid: string }; // must match route param
-};
+  params: { cid: string }; // must match route param name exactly
+}) {
+  const { cid } = params; // extract the route param
 
-export default function CoursesLayout({ children, params }: CoursesLayoutProps) {
-  const { cid } = params; // extract cid directly
   return (
     <div id="wd-courses">
       <h2>Courses {cid}</h2>
@@ -15,8 +17,12 @@ export default function CoursesLayout({ children, params }: CoursesLayoutProps) 
       <table>
         <tbody>
           <tr>
-            <td valign="top" width={200}><CourseNavigation /></td>
-            <td valign="top" width="100%">{children}</td>
+            <td valign="top" width={200}>
+              <CourseNavigation />
+            </td>
+            <td valign="top" width="100%">
+              {children}
+            </td>
           </tr>
         </tbody>
       </table>
